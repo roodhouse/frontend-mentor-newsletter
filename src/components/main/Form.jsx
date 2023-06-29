aa126426202221e665af943759c43179c650d2eb
roodhouse <rughjm@gmail.com>
2023-06-29 17:04:24 -0500

    main page done styling

--- src/components/main/Form.jsx
+++ src/components/main/Form.jsx
@@ -4,15 +4,16 @@ function Form() {
   return (
     <>
       <div id="formContainer">
-        <div id="emailHead">
+        <div id="emailHead" className='font-Roboto text-xs font-bold leading-[150%] text-darkSlateGray mb-2 flex justify-between'>
           <p>Email address</p>
+          <p className='text-tomato invisible'>Valid Email Required</p>
         </div>
         <div id="form">
           <form>
-            <div id="emailInputContainer">
-              <input type="email" id='email' name='email' placeholder='email@company.com' />
+            <div id="emailInputContainer" className='mb-6 rounded-[8px] border border-solid border-[rgba(25,24,43,0.25)] w-full'>
+              <input type="email" id='email' name='email' placeholder='email@company.com' className='w-full py-4 pl-6 rounded-[8px]' />
             </div>
-            <div id="submitContainer">
+            <div id="submitContainer" className='cursor-pointer bg-darkSlateGray text-white rounded-[8px] flex justify-center items-center py-[18px]'>
               <input type="submit" id='submit' name='submit' value='Subscribe to monthly newsletter' />
             </div>
           </form>
